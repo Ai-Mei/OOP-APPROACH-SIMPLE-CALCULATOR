649dded2fc2b4b0ebc87306a41c23b31c4bc69d7
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-21 11:51:15 +0800

    Initial commit.

---
+++ SIMPLECALCULATOR.py
@@ -0,0 +1,4 @@
+# Ask the user for 2 innput numbers.
+# Ask the user for operation to perform.
+# Input result.
+# Ask the user if they want to use calculator again.
---
+++ USERINTERFACE.py
@@ -0,0 +1,5 @@
+# Asks input from user.
+# Converts the input to float.
+# Ask for operation.
+# Prints results.
+# Asks the user to either repeat the program.
