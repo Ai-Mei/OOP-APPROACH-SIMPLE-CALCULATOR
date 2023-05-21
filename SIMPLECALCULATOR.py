e72439cafbfab2160aceb98d3492ec4f2f8b2451
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-21 13:17:29 +0800

    Prints the answer

--- SIMPLECALCULATOR.py
+++ SIMPLECALCULATOR.py
@@ -8,6 +8,11 @@ operation = Operation()
 # Ask the user for 2 innput numbers.
 num_1 = ui.UserInput()
 num_2 = ui.UserInput()
+
 # Ask the user for operation to perform.
-# Input result.
+operation_choice = ui.OperationChoice()
+
+# Prints result.
+ui.Result(num_1, num_2, operation_choice, operation)
+
 # Ask the user if they want to use calculator again.
--- USERINTERFACE.py
+++ USERINTERFACE.py
@@ -1,7 +1,12 @@
+from OPERATIONS import Operation
+operation = Operation
+
 class UserInterface:
+    # Asks input from user.
     def UserInput(self):
         while True:
             number = input("Enter a number: ")
+            # Converts the input to float.
             try:
                 number = float(number)
                 if number.is_integer():
@@ -12,9 +17,28 @@ class UserInterface:
                 continue
         return number
 
-# Asks input from user.
+    # Ask for operation.
+    def OperationChoice(self):
+        operation = input("Please select an operation to perform + for addition, - for subtraction, * for multiplication, / for division: ")
+        return operation
+    
+    # Prints results.
+    def Result(self, num_1, num_2, operation_choice, operation):
+        while True:
+            if operation_choice == "+":
+                result = operation.Addition(num_1, num_2)
+                break
+            elif operation_choice == "-":
+                result = operation.Subtraction(num_1, num_2)
+                break
+            elif operation_choice == "*":
+                result = operation.Multiplication(num_1, num_2)
+                break
+            elif operation_choice == "/":
+                result = operation.Division(num_1, num_2)
+                break
+            else:
+                print("⚠️ Invalid operation selected.")
+                operation_choice = self.OperationChoice()
 
-# Converts the input to float.
-# Ask for operation.
-# Prints results.
-# Asks the user to either repeat the program.
+    # Asks the user to either repeat the program.
