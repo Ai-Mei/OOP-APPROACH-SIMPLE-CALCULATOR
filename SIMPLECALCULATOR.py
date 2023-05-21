070748a7324a1dc7118ab132fd37372637ee47d0
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-21 12:39:26 +0800

    Input message accepts input from user.

--- SIMPLECALCULATOR.py
+++ SIMPLECALCULATOR.py
@@ -1,4 +1,13 @@
+#import the two classes.
+from USERINTERFACE import UserInterface
+from OPERATIONS import Operation
+
+ui = UserInterface()
+operation = Operation()
+
 # Ask the user for 2 innput numbers.
+num_1 = ui.UserInput()
+num_2 = ui.UserInput()
 # Ask the user for operation to perform.
 # Input result.
 # Ask the user if they want to use calculator again.
--- USERINTERFACE.py
+++ USERINTERFACE.py
@@ -1,4 +1,19 @@
+class UserInterface:
+    def UserInput(self):
+        while True:
+            number = input("Enter a number: ")
+            try:
+                number = float(number)
+                if number.is_integer():
+                    number = int(number)
+                break
+            except ValueError:
+                print("⚠️ Invalid input. Please enter a valid number.")
+                continue
+        return number
+
 # Asks input from user.
+
 # Converts the input to float.
 # Ask for operation.
 # Prints results.
