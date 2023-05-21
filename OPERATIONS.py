49bcaf9041a9d75572406a2ed385986e5d3c5870
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-21 12:22:13 +0800

    The calculation class is done.

--- OPERATIONS.py
+++ OPERATIONS.py
@@ -1,4 +1,27 @@
-# Addition
-# Subtraction
-# Multiplication
-# Division
+class Operation:
+    # Addition
+    def Addition(self, num_1, num_2):
+        answer = num_1 + num_2
+        print("The sum of the two numbers:", answer)
+    
+    # Subtraction
+    def Subtraction(self, num_1, num_2):
+        answer = num_1 - num_2
+        print("The difference of the two numbers:", answer)
+    
+    # Multiplication
+    def Multiplication(self, num_1, num_2):
+        answer = num_1 * num_2
+        print("The product of the two numbers:", answer)
+
+    # Division
+    def Division(self, num_1, num_2):
+        try:
+            answer = num_1 / num_2
+            print("The quotient of the two numbers:", answer)
+        except ZeroDivisionError:
+            print("⚠️ Error, the number is divided by 0.")
+        
+
+
+   
